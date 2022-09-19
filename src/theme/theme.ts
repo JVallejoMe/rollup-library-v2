b2c9fb98ffb8443b85f0bc8e2ec0b2df7bdbd573
Jose Vallejo <Jose.Vallejo@tranzact.net>
2022-09-19 12:55:01 -0500

    Add dark theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,8 +2,8 @@ import { createTheme, ThemeOptions } from "@mui/material";
 import { TypographyVariantsOptions } from "@mui/material";
 import { PaletteOptions } from "@mui/material";
 
-const lightPalette: PaletteOptions = {
-  mode: "light",
+const darkPalette: PaletteOptions = {
+  mode: "dark",
   primary: {
     main: "#67be23",
     contrastText: "#fff",
@@ -13,15 +13,15 @@ const lightPalette: PaletteOptions = {
     contrastText: "#fff",
   },
   background: {
-    default: "#f0f0f0",
-    paper: "#ffffff",
+    default: "#212121",
+    paper: "#242424",
   },
   success: {
     main: "#67be23",
     contrastText: "#fff",
   },
   error: {
-    main: "#fa541c",
+    main: "#ee2a1e",
     contrastText: "#fff",
   },
   warning: {
@@ -29,14 +29,14 @@ const lightPalette: PaletteOptions = {
     contrastText: "#fff",
   },
   info: {
-    main: "#0b82f0",
+    main: "#1890ff",
     contrastText: "#fff",
   },
   divider: "rgba(0,0,0,0)",
   text: {
-    primary: "#626262",
-    secondary: "#9f9f9f",
-    disabled: "#c1c1c1",
+    primary: "#fff",
+    secondary: "rgba(255,255,255,0.7)",
+    disabled: "#d1d1d1",
   },
 };
 
@@ -65,23 +65,21 @@ const commonThemeProperties: ThemeOptions = {
   },
 };
 
-const LightTheme = createTheme({
+const DarkTheme = createTheme({
   ...commonThemeProperties,
-  palette: lightPalette,
+  palette: darkPalette,
   components: {
-    MuiAppBar: {
-      styleOverrides: {
-        colorDefault: {
-          backgroundColor: "#fff",
-        },
-      },
-    },
     MuiPaper: {
       styleOverrides: {
         root: {
           backgroundImage:
-            "linear-gradient(rgba(255, 255, 255, 0.01), rgba(255, 255, 255, 0.01))",
+            "linear-gradient(rgba(255, 255, 255, 0.025), rgba(255, 255, 255, 0.025))",
         },
+      },
+    },
+    MuiAppBar: {
+      defaultProps: {
+        color: "transparent",
       },
     },
     MuiTypography: {
@@ -95,4 +93,4 @@ const LightTheme = createTheme({
   },
 });
 
-export { LightTheme };
+export { DarkTheme };
